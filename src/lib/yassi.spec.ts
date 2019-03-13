055f248a4a8df712aa924b571340287357c8995d
Kfir Erez <kfir@zebra-med.com>
2019-03-13 15:34:51 +0200

    Test for select a property that does not exists in the store returns undefined. Test for select a property that was changed after it and read it again to see the actual change.

--- src/lib/yassi.spec.ts
+++ src/lib/yassi.spec.ts
@@ -13,19 +13,19 @@ import {select, yassit} from './yassi';
 // }
 
 class TestSource {
-  @yassit('Test.srcNumProp1')
+  @yassit('TestSource.srcNumProp1')
   numProp1: number;
 
-  @yassit('Test.srcNumProp2')
+  @yassit('TestSource.srcNumProp2')
   numProp2: number = 2;
 
   numProp3: number = 3;
 
-  @yassit('Test.srcStringProp4')
+  @yassit('TestSource.srcStringProp4')
   strProp4: string = 'hey there';
 }
 
-test('Simple instance with Yassi but works as without', (t) => {
+test('object instance with Yassi but works as without', (t) => {
   const test1 = new TestSource();
   t.is(test1.numProp1, undefined);
   t.is(test1.numProp2, 2);
@@ -33,11 +33,11 @@ test('Simple instance with Yassi but works as without', (t) => {
   t.is(test1.strProp4, 'hey there');
 });
 
-test("Simple Component A's number property is selected by Component B", (t) => {
+test("object A's property is selected by object B", (t) => {
   class TestDest {
-    @select('Test.srcNumProp1') extProp1;
-    @select('Test.srcNumProp2') extProp2;
-    @select('Test.srcStringProp4') extProp3;
+    @select('TestSource.srcNumProp1') extProp1;
+    @select('TestSource.srcNumProp2') extProp2;
+    @select('TestSource.srcStringProp4') extProp3;
 
     myProp1: number = 10;
   }
@@ -48,4 +48,28 @@ test("Simple Component A's number property is selected by Component B", (t) => {
   t.is(test1.numProp2, test2.extProp2);
   t.is(test1.numProp3, 3);
   t.is(test1.strProp4, test2.extProp3);
+});
+
+test('select property that does not exists in the store and expect undefined follow js behaviour',(t) => {
+  class TestDest {
+    @select('NoKeyFound') notFoundProp;
+    @select('TestSource.srcNumProp1') prop1;
+  }
+
+  const test1 = new TestSource();
+  const test2 = new TestDest();
+  t.is(test1.numProp1, test2.prop1);
+  t.is(test2.notFoundProp, undefined);
+});
+
+test("select A's property from obj B, change it and read it again. Expect to see the change", (t) => {
+  class TestDest1 {
+    @select('TestSource.srcNumProp1') prop1;
+  }
+
+  const test1 = new TestSource();
+  const test2 = new TestDest1();
+  t.is(test1.numProp1, test2.prop1);
+  test1.numProp1 = 1000;
+  t.is(test2.prop1, 1000);
 });
